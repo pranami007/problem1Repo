5a563bae0f47c7b6661e6d0606a42d126947264d
pranami raviraj <praviraj4168@gmail.com>
2020-12-21 13:32:04 +0530

    Program 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String args[]) {
+		int[] arr = { 2, 0, 6, 1, 5, 3, 7,1,2,3,4,6,5,7,8,9,10};
+ 
+        System.out.println("Output : " +result(arr));
+	}
+	
+	public static int result(int[] arr)
+    {
+        Set<Integer> S = IntStream.of(arr)
+                                .boxed()
+                                .collect(Collectors.toSet());
+        int maxLen = 1;
+        for (int e: arr)
+        {
+            if (!S.contains(e - 1))
+            {
+                int len = 1;
+                while (S.contains(e + len)) {
+                    len++;
+                }
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+}
